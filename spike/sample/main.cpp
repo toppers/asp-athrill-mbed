0a23478566693da46280e2b2a4c914e5473a2588
tmori@esm.co.jp <tmori@esm.co.jp>
2018-12-24 17:19:26 +0900

    add server sample

--- spike/sample/main.cpp
+++ spike/sample/main.cpp
@@ -2,26 +2,54 @@
 #include "test_reg.h"
 #include "test_serial.h"
 #include "Socket/TCPSocketConnection.h"
+#include "Socket/TCPSocketServer.h"
+#include <string.h>
 
 unsigned char stack_data[STACK_SIZE] __attribute__ ((section(".bss_noclr")));
 sys_addr athrill_device_func_call __attribute__ ((section(".athrill_device_section")));
 
 TCPSocketConnection *sock_conn;
+TCPSocketServer *sock_srv;
+
+unsigned int test_mode  __attribute__ ((section(".bss_noclr")));
 
 int main(void)
 {
 	char buffer[256];
-	sock_conn = new TCPSocketConnection();
 
-	(void)sock_conn->connect("127.0.0.1", 50005);
+	test_print("main:enter\n");
 
-	sock_conn->send("This is a test data\n", sizeof("This is a test data\n"));
+	if (test_mode == 0) {
+		sock_conn = new TCPSocketConnection();
 
-	sock_conn->receive(buffer, sizeof(buffer));
-	test_print(buffer);
-	
-	sock_conn->close(true);
+		(void)sock_conn->connect("127.0.0.1", 50005);
 
-	delete sock_conn;
+		sock_conn->send((char*)"This is a test data\n", sizeof("This is a test data\n"));
+
+		sock_conn->receive(buffer, sizeof(buffer));
+		test_print(buffer);
+		test_print("\n");
+
+		sock_conn->close(true);
+
+		delete sock_conn;
+	}
+	else {
+		TCPSocketConnection new_conn;
+		sock_srv = new TCPSocketServer();
+		sock_srv->bind(50005);
+		sock_srv->listen();
+		sock_srv->accept(new_conn);
+
+		new_conn.receive(buffer, sizeof(buffer));
+		test_print(buffer);
+		test_print("\n");
+
+		new_conn.send(buffer, strlen(buffer));
+		new_conn.close();
+
+		sock_srv->close(true);
+		delete sock_srv;
+	}
 	return 0;
 }
