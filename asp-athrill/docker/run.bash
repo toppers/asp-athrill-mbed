4009eacd4c09e153771ce9bbe0b1aa3faab9476a
Takashi Mori <tmori@esm.co.jp>
2021-04-18 09:54:32 +0900

    add hakoniwa command

--- asp-athrill/docker/run.bash
+++ asp-athrill/docker/run.bash
@@ -5,4 +5,5 @@ DOCKER_IMAGE=asp-athrill/v850:v1.0.0
 
 sudo docker run \
 	-v ${WORKSPACE_DIR}:/root/workspace/asp \
+	-v ${WORKSPACE_DIR}/docker/bin:/root/workspace/bin \
 	-it --rm --net host --name asp-athrill ${DOCKER_IMAGE} 
